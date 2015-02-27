f9f4b8da8438b1b9d30c948f1195436ae50d8569
Wasknijper <mudkippers@hotmail.com>
2015-02-27 12:33:34 +0100

    trying to get saving to work

--- client/templates/art/drawpage.js
+++ client/templates/art/drawpage.js
@@ -11,3 +11,18 @@ Template.drawPage.helpers({
 	}
 		
 })
+
+Template.artList.events({  
+  'click #Save': function(e) {
+  	e.preventDefault();
+
+  	var savedWork = {
+  		artUrl: artPiece[0].webImage.url.replace("s0", "s600"),
+  		drawing: $('SketchPad')toDataURL("image/png", 1.0);
+  	};
+
+  	console.log('saved I hope?')
+
+  	Artworks.insert(savedWork);
+  }
+});
